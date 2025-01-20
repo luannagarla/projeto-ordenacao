993a1bb3f777f7e78e0d42110082adbbd29fcd15
luanna.garla <luanna.garla@agritel.com.br>
2025-01-20 17:23:19 -0300

    Códigos de geração de pratos

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # Atividade de ordenação - bubbleSort e quickSort
 
-Projeto desenvolvido como parte da disciplina de Estrutura de Ddos do curso de Ciência de Dados e Inteligência Artificial na Universidade Estadual de Londrina (UEL). O objetivo é consolidar conceitos fundamentais da disciplina relacionado à ordenação com BubbleSort e QuickSort.
-
----
-
-**Descrição do trabalho** 
-
 Joãozinho está planejando abrir um restaurante especializado em comida britânica e pediu sua ajuda para priorizar os pratos do cardápio. Como seu prato favorito tem a palavra “Bubble” no nome, ele acredita que o algoritmo Bubble Sort seria o mais adequado para essa tarefa. No entanto, como você conhece algoritmos mais eficientes, pretende implementar outro para demonstrar uma solução mais otimizada.
 
 ---
 
 **:dart: Objetivos** 
 
-- [X]   Recepção de 0 < k < 3 ⋅ 105 pratos (300k);
+- [X]   Recepção de 0 < k < 3 ⋅ 10^5 pratos (300k);
 - [X]   Implementação do método de ordenação Quicksort;
 - [X]   Implementação do método de ordenação Quicksort;
 - [X]   Nenhum prato tem a mesma prioridade e tempo de preparo;
+- [X]  A prioridade é um inteiro 0 < p < 3 ⋅ 10^5, o tempo de preparo (em minutos) é um inteiro 0 < t < 10^3 (1k);
 - [X]   Compare os métodos de ordenação e faça uma breve explicação sobre a escolha de pivôs no Quicksort.
+
+---
+**:dart: Comaprações do código** 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
 #include <random>
 #include <ctime>
+#include <string> 
+#include <sstream> 
 
 using namespace std;
 
@@ -24,12 +24,20 @@ void quickSort(vector<Prato> &pratos, int low, int high)
 
 string gerarNomePrato(int indice)
 {
-    
+    static vector<string> nomes = {
+        "fish-and-chips", "full-english-breakfast", "bubble-and-squeak", "scones", "steak-and-ale-pie",
+        "shepherds-pie", "ploughman", "toad-in-the-hole", "black-pudding", "wellington-beef", "scotch-egg",
+        "cornish-pasty", "yorkshire-pudding", "sponge-cake", "chicken-tikka-masala", "bangers-and-mash"};
+
+    return nomes[indice % nomes.size()];
+    // return nome + "-" + to_string(indice); //não tenho a biblioteca que faça isso, preciso achar
 }
 
 int gerarNumeroAleatorio(int min, int max)
 {
-    
+    static mt19937 rng(time(0)); // random
+    uniform_int_distribution<int> dist(min, max);
+    return dist(rng);
 }
 
 int main()
@@ -39,7 +47,18 @@ int main()
 
     for (int i = 0; i < n; ++i)
     {
-       //criar pratos
+        pratos[i].prioridade = gerarNumeroAleatorio(1, 300000); // Prioridade - 0 < k < 300k
+        pratos[i].tempo = gerarNumeroAleatorio(1, 1000);        // Tempo - 0 < t < 10k
+        pratos[i].nome = gerarNomePrato(i);                     // Nome
+    }
+
+    cout << "Pratos gerados:" << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "Prato " << i + 1 << ": "
+             << "Nome: " << pratos[i].nome << ", "
+             << "Prioridade: " << pratos[i].prioridade << ", "
+             << "Tempo: " << pratos[i].tempo << " minutos" << endl;
     }
 
     /*Ordene em ordem decrescente de acordo com a prioridade, se a prioridade for a mesma,
@@ -52,4 +71,4 @@ t < 103, o nome é uma string de no máximo 50 caracteres, sem espaço.*/
     return 0;
 }
 
-//Compare os métodos de ordenação e faça uma breve explicação sobre a escolha de pivôs no Quicksort.
+// Compare os métodos de ordenação e faça uma breve explicação sobre a escolha de pivôs no Quicksort.
